c83a732fb086d63aaacce829dba0e1f2facb4295
Nishit Mangal <nishitmangal5@gmail.com>
2024-04-07 04:03:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { callApiToRegisterUser } from './apiHandler';
 
 function App() {
+  const [name, setName] = useState("")
+  const registerName = async ()=>{
+    let response = await callApiToRegisterUser(name)
+    if(!response)
+      alert('Failed to create the Name')
+    setName('')
+    return
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder='Name'
+        value={name}
+        onChange={(e)=>setName(e.target.value)}
+      >
+      </input>
+      <button onClick={registerName}>Register</button>
     </div>
   );
 }
---
+++ src/apiHandler.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+let url = "http://13.233.53.159:8000/visited"
+// let url = "http://localhost:8000/visited";
+
+export async function callApiToRegisterUser(name) {
+  let data = {
+    name: name,
+  };
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: url,
+    data: data,
+  };
+  try {
+    let response = await axios(config);
+    console.log(response.data);
+    if(response.data.status!=='Success'){
+        throw response.data.data
+    }
+    return response.data.data;
+  } catch (err) {
+    console.log(`Error ${err}`)
+    return null;
+  }
+}
